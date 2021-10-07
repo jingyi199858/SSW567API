b1af4fc95d9aa34d3aeee2aa92cc6e8ce1c2f40f
jing <jingdakai@gmail.com>
2021-10-07 18:39:28 -0400

    Mock all rest api tests

--- GitHubAPITest.py
+++ GitHubAPITest.py
@@ -1,4 +1,5 @@
-import unittest.mock
+import unittest
+from unittest import mock
 import requests
 
 from GitHubAPI import githubAPI
@@ -13,7 +14,24 @@ class GitHubAPITest(unittest.TestCase):
         self.assertEqual(githubAPI(1234567), "The input is not string", "Should enter input string")
 
     def testFirstOfMine(self):
+        succes_return = mock.Mock(
+            return_value=['Repo Name: CS513Stevens Commits Number:3', 'Repo Name: cs546coursework Commits Number:7',
+                          'Repo Name: CS557Stevens Commits Number:2', 'Repo Name: CS559HW2 Commits Number:7',
+                          'Repo Name: CS559ML Commits Number:1', 'Repo Name: EE629 Commits Number:3',
+                          'Repo Name: GadgetClear Commits Number:4', 'Repo Name: jingyi199858 Commits Number:1',
+                          'Repo Name: photoModule Commits Number:1', 'Repo Name: SSW555Gedcom Commits Number:30',
+                          'Repo Name: SSW567 Commits Number:5', 'Repo Name: SSW567API Commits Number:7',
+                          'Repo Name: ssw567hw2 Commits Number:7', 'Repo Name: taylorfit Commits Number:30',
+                          'Repo Name: TeamAssignment2 Commits Number:13'])
+        githubAPI = succes_return
         self.assertEqual(githubAPI("jingyi199858")[0], "Repo Name: CS513Stevens Commits Number:3", "Incorrect info retrieved")
 
     def testResponse(self):
+        returned_value = mock.Mock(return_value=404)
+        githubAPI = returned_value
         self.assertEqual(githubAPI(""), 404, "Can't retrieve info")
+
+    def testMock(self):
+        succes_return = mock.Mock(return_value=['Repo Name: CS513Stevens Commits Number:3', 'Repo Name: cs546coursework Commits Number:7', 'Repo Name: CS557Stevens Commits Number:2', 'Repo Name: CS559HW2 Commits Number:7', 'Repo Name: CS559ML Commits Number:1', 'Repo Name: EE629 Commits Number:3', 'Repo Name: GadgetClear Commits Number:4', 'Repo Name: jingyi199858 Commits Number:1', 'Repo Name: photoModule Commits Number:1', 'Repo Name: SSW555Gedcom Commits Number:30', 'Repo Name: SSW567 Commits Number:5', 'Repo Name: SSW567API Commits Number:7', 'Repo Name: ssw567hw2 Commits Number:7', 'Repo Name: taylorfit Commits Number:30', 'Repo Name: TeamAssignment2 Commits Number:13'])
+        githubAPI = succes_return
+        self.assertEqual(githubAPI("jingyi199858")[0], "Repo Name: CS513Stevens Commits Number:3",)
